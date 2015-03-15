bfa6187d8af077d008943d3d0f89e338541f2fcc
Reinis Priednieks <priednis@gmail.com>
2015-03-15 22:32:19 +0100

    Builder gets energy when needed

--- builder.js
+++ builder.js
@@ -1,30 +1,16 @@
 /**
  * Created by Reinis on 15.03.2015..
  */
-module.exports = function (creep) {
+module.exports = function (creep, spawn) {
     var role = creep.memory.role;
     var target;
 
-    if (role == 'builder'){
-        target = creep.pos.findClosest(Game.CONSTRUCTION_SITES);
-        if(target) {
-            creep.build(target);
-            if (!creep.pos.isNearTo(target)) {
-                creep.moveTo(target);
-            }
-        }
+    if(creep.energy == 0) {
+        creep.moveTo(spawn);
+        spawn.transferEnergy(creep);
     } else {
-        target = creep.pos.findClosest(Game.STRUCTURES, {
-            filter: function(object) {
-                return (object.hitsMax - object.hits) > 200;
-            }
-        });
-        if(target) {
-            creep.repair(target);
-            if (!creep.pos.isNearTo(target)) {
-                creep.moveTo(target);
-            }
-        } else {
+
+        if (role == 'builder'){
             target = creep.pos.findClosest(Game.CONSTRUCTION_SITES);
             if(target) {
                 creep.build(target);
@@ -32,7 +18,27 @@ module.exports = function (creep) {
                     creep.moveTo(target);
                 }
             }
+        } else {
+            target = creep.pos.findClosest(Game.STRUCTURES, {
+                filter: function(object) {
+                    return (object.hitsMax - object.hits) > 200;
+                }
+            });
+            if(target) {
+                creep.repair(target);
+                if (!creep.pos.isNearTo(target)) {
+                    creep.moveTo(target);
+                }
+            } else {
+                target = creep.pos.findClosest(Game.CONSTRUCTION_SITES);
+                if(target) {
+                    creep.build(target);
+                    if (!creep.pos.isNearTo(target)) {
+                        creep.moveTo(target);
+                    }
+                }
+            }
         }
-    }
 
+    }
 };
