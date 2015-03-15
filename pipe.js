b272b2f668f163667ac9e48727c51ef7fb3e0c66
Reinis Priednieks <priednis@gmail.com>
2015-03-15 03:46:31 +0100

    The last pipe should deliver energy if no carrier is available. If pipe has energy and is under attack return to spawn.

--- pipe.js
+++ pipe.js
@@ -2,7 +2,9 @@
  * Created by Reinis on 15.03.2015..
  */
 module.exports = function(creep) {
-    creep.moveTo(creep.memory.target.x, creep.memory.target.y);
+    if (!creep.memory.deliveringEnergy && !creep.memory.pickingUpEnergy) {
+        creep.moveTo(creep.memory.target.x, creep.memory.target.y);
+    }
 
     if (creep.memory.role == 'pipehead') {
         var target = creep.pos.findClosest(Game.SOURCES, {maxOps: 10, ignoreCreeps: true});
@@ -20,7 +22,11 @@ module.exports = function(creep) {
         // check for dropped energy nearby
         var droppedEnergy = creep.pos.findClosest(Game.DROPPED_ENERGY);
         if (creep.pos.isNearTo(droppedEnergy)) {
+            creep.memory.pickingUpEnergy = true;
+            creep.moveTo(droppedEnergy);
             creep.pickup(droppedEnergy);
+        } else {
+            creep.memory.pickingUpEnergy = false;
         }
 
         if (creep.ticksToLive == 60) {
@@ -34,7 +40,23 @@ module.exports = function(creep) {
         var nextCarrierName = Memory.pipeToSource[pipeLocation - 1].name;
         creep.transferEnergy(Game.creeps[nextCarrierName]);
     } else {
+        // TODO: If no carriers exist, bring the energy back to Spawn
+        if (creep.energy < creep.energyCapacity) {
+            creep.memory.deliveringEnergy = false;
+        } else {
+            creep.memory.deliveringEnergy = true;
+            creep.moveTo(Game.spawns.Spawn1);
+            creep.transferEnergy(Game.spawns.Spawn1);
+        }
+    }
+
+    // If under attack and am carrying some energy, return to Spawn (and hope for the best :) )
+    if (creep.hits < creep.hitsMax && creep.energy > 0) {
+        creep.memory.deliveringEnergy = true;
+        creep.moveTo(Game.spawns.Spawn1);
         creep.transferEnergy(Game.spawns.Spawn1);
+    } else {
+        creep.memory.deliveringEnergy = false;
     }
 
     /*
